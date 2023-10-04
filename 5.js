d3e785567708a67f6d842bd4956c84f1916ff8cc
unknown <bpomogaibo.gmail.com>
2023-10-04 14:39:14 +0300

    add js-23

--- 5.js
+++ 5.js
@@ -4,10 +4,18 @@
 // Клас BankTransfer представляє собою систему для здійснення банківських переказів
 class BankTransfer {
   // Зробіть метод initiateTransfer, який приймає amount та відповідає за ініціювання банківського переказу
+  initiateTransfer(amount) {
+    const calculatedAmount = this.calculateFee(amount);
+    console.log(`Ініціюємо банківський переказ: $${calculatedAmount}`);
+  }
   // Він приймає суму переказу як параметр
   // Для ініціювання банківського переказу спершу обчислюється сума з урахуванням комісії calculatedAmount = this.calculateFee(amount)
   // Виводимо інформацію про ініціювання банківського переказу Ініціюємо банківський переказ: $${calculatedAmount}
+
   // Зробіть метод calculateFee, який відповідає за розрахунок комісії за переказ
+  calculateFee(amount) {
+    return amount * 1.02;
+  }
   // Він приймає amount переказу як параметр та повертає число після розрахування комісії
   // Логіка розрахунку комісії за переказ amount * 1.02
   // Припустимо, комісія становить 2% від суми переказу
@@ -16,6 +24,10 @@ class BankTransfer {
 // Клас WalletTransfer представляє собою систему для здійснення переказів з гаманця
 class WalletTransfer {
   // Створіть метод processTransfer, який відповідає за здійснення переказу з гаманця
+
+  processTransfer(amount) {
+    console.log(`Здійснюємо переказ з гаманця: $${amount}`);
+  }
   // Він приймає суму переказу як параметр
   // Виводимо інформацію про здійснення переказу з гаманця Здійснюємо переказ з гаманця: $${amount}
 }
@@ -24,10 +36,21 @@ class WalletTransfer {
 // методами WalletTransfer так, ніби це BankTransfer.
 class TransferAdapter {
   // Робимо конструктор, що приймає об'єкт transferSystem типу WalletTransfer
+  constructor(transferSystem) {
+    this.transferSystem = transferSystem(new WalletTransfer());
+  }
   // Зберігаємо посилання на об'єкт WalletTransfer у властивості transferSystem
   // Робимо метод initiateTransfer, який адаптує API WalletTransfer до API BankTransfer.
+  initiateTransfer(amount) {
+    const calculatedAmount = this.calculateFee(amount);
+    return amount;
+  }
   // Він приймає amount як аргумент та повертає результат виконання переказу.
+
   // Викликаємо допоміжний метод calculateFee для обчислення комісії за переказ та результат записуєм в константу calculatedAmount
+  calculateFee(amount) {
+    return amount * 1.02;
+  }
   // Викликаємо метод processTransfer об'єкту WalletTransfer з calculatedAmount.
   // В результаті повертаємо результат виконання переказу.
   // Створюємо метод calculateFee, що приймає amount та обчислює суму комісії за переказ amount * 1.2, засновуючись на вхідній сумі.
@@ -42,3 +65,64 @@ console.log("Завдання 5 ====================================");
 
 // const purchase2 = new BankTransfer();
 // purchase2.initiateTransfer(10);
+
+console.log(`//======`);
+
+//==== Система електронних платежів з власним API
+class ElectronicPaymentSystem {
+  makePayment(amount) {
+    const convertedAmount = this.convertAmount(amount);
+    console.log(`Making electronic payment: $${convertedAmount}`);
+  }
+
+  convertAmount(amount) {
+    //=== Логіка конвертації суми платежу
+    return amount * 1.2; //== Припустимо, що необхідна конвертація у відсотках
+  }
+}
+
+class OtherPaymentSystem {
+  submit(amount) {
+    console.log(`Submitting payment request: $${amount}`);
+  }
+}
+
+class PaymentAdapter {
+  constructor(paymentSystem) {
+    this.paymentSystem = paymentSystem;
+  }
+
+  makePayment(amount) {
+    const convertedAmount = this.convertAmount(amount);
+    this.paymentSystem.submit(convertedAmount);
+  }
+
+  convertAmount(amount) {
+    return amount * 1.2;
+  }
+}
+
+class Order {
+  constructor(amount) {
+    this.amount = amount;
+
+    if (amount < 100) {
+      this.paymentSystem = new PaymentAdapter(new OtherPaymentSystem());
+    } else {
+      this.paymentSystem = new ElectronicPaymentSystem();
+    }
+  }
+
+  makePayment() {
+    return this.paymentSystem.makePayment(this.amount);
+  }
+}
+
+const order1 = new Order(1000);
+order1.makePayment();
+
+const order2 = new Order(10);
+order2.makePayment();
+
+// const electronicPaymentSystem = new ElectronicPaymentSystem();
+// electronicPaymentSystem.makePayment(100);
--- 8.js
+++ 8.js
@@ -4,30 +4,99 @@
 // Клас User відповідає за користувача, який може відправляти повідомлення.
 class User {
   // Створюємо конструктор класу, який приймає name та messenger як параметри та ініціалізує їх
+  constructor(name, messenger) {
+    this.name = name;
+    this.messenger = messenger;
+  }
   // Робимо метод sendMessage який відправляє повідомлення за допомогою відповідного месенджера, та виводить в консоль `${this.name} відправив повідомлення ${message}`.
+  sendMessage(message) {
+    console.log(`${this.name} відправив повідомлення ${message}`);
+    return this.messenger.sendMessage(message);
+  }
   // Він приймає один параметр - message - повідомлення, яке потрібно відправити за допомогою методу sendMessage.
   // Метод receiveMessage приймає аргументи user,message та виводить в консоль ${this.name} отримав повідомлення від ${user.name}: ${message}
+  receiveMessage(user, message) {
+    console.log(
+      `${this.name} отримав повідомлення від ${user.name}: ${message}`
+    );
+  }
 }
 
 // Клас SMSMessenger відповідає за відправку повідомлень за допомогою SMS.
 class SMSMessenger {
   // Створюємо статичний метод sendMessage який приймає один параметр - message, та виводить в консоль `Відправлено SMS: ${message}`
+  static sendMessage(message) {
+    console.log(`Відправлено SMS: ${message}`);
+  }
 }
 
 // Клас EmailMessenger відповідає за відправку повідомлень за допомогою Email.
 class EmailMessenger {
   // Створюємо статичний метод sendMessage який приймає один параметр - message, та виводить в консоль `Відправлено Email: ${message}`
+  static sendMessage(message) {
+    console.log(`Відправлено Email: ${message}`);
+  }
 }
-
 console.log("Завдання 7 ====================================");
 // Після виконання розкоментуйте код нижче
 
 // Створюємо двох користувачів - John та Jane - які відправляють повідомлення за допомогою різних месенджерів.
-// const john = new User("John", SMSMessenger);
-// const jane = new User("Jane", EmailMessenger);
+const john = new User("John", SMSMessenger);
+const jane = new User("Jane", EmailMessenger);
 
 // John відправляє повідомлення за допомогою SMS.
-// john.sendMessage("Привіт!"); // Виведе: Відправлено SMS: [John]: Привіт!
+john.sendMessage("Привіт!"); // Виведе: Відправлено SMS: [John]: Привіт!
 
 // Jane відправляє повідомлення за допомогою Email.
-// jane.sendMessage("Привіт!"); // Виведе: Відправлено Email: [Jane]: Привіт!
+jane.sendMessage("Привіт!"); // Виведе: Відправлено Email: [Jane]: Привіт!
+
+console.log(`//=====`);
+
+// class User15 {
+//   constructor(name, chat) {
+//     this.name = name;
+//     this.chat = chat;
+//   }
+
+//   sendMessage(message) {
+//     console.log(`${this.name} відправив повідомлення ${message}`);
+//     return this.chat.sendMessage(this, message);
+//   }
+
+//   //Прийняття повідомлення від іншого користувача
+//   receiveMessage(sender, message) {
+//     console.log(
+//       `${this.name} отримав повідомлення від ${sender.name}: ${message}`
+//     );
+//   }
+// }
+
+// class Chat {
+//   users = [];
+
+//   //Додавання користувача до чату
+//   addUser(user) {
+//     this.users.push(user);
+//   }
+
+//   //Відправлення повідомлення в чат
+//   sendMessage(sender, message) {
+//     for (const user of this.users) {
+//       if (user !== sender) {
+//         user.receiveMessage(sender, message);
+//       }
+//     }
+//   }
+// }
+
+// const chatMediator = new Chat();
+
+// const user01 = new User15("John", chatMediator);
+// const user02 = new User15("Jane", chatMediator);
+// const user03 = new User15("Mike", chatMediator);
+
+// chatMediator.addUser(user01);
+// chatMediator.addUser(user02);
+// chatMediator.addUser(user03);
+
+// user01.sendMessage("Привіт, всім");
